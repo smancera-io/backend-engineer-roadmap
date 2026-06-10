ac637028058575841f70e86c4b3fa5a325959ba8
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-09 19:54:42 -0500

    feat: inizialize 8 variables

--- 1.1Syntax/Main.java
+++ 1.1Syntax/Main.java
@@ -1,3 +1,13 @@
-public  {
-    
+public class Main {
+    public static void main(String[] args) {
+        /* Inizialize 8 variables */
+        byte number1 = 5;
+        float number2 = 120.2;
+        int number3 = 520000;
+        double number4 = 3.141516;
+        long number5 = 10000000000L;
+        char letter = 'S';
+        String word = "Samuel";
+        boolean flag = true;
+    }
 }
