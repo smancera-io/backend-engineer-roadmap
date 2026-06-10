797d8e79c98c1975f8682b81c3d9920873dd8c5b
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-09 20:23:43 -0500

    feat: cast from a double to a int number

--- 1.1Syntax/Main.java
+++ 1.1Syntax/Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         /* Inizialize 8 variables */
         byte number1 = 5;
-        float number2 = 120.2;
+        float number2 = 12.2f;
         int number3 = 520000;
         double number4 = 3.141516;
         long number5 = 10000000000L;
         char letter = 'S';
         String word = "Samuel";
         boolean flag = true;
+
+        /* Casting double number to int number */
+        int casting = (int) number4;
+        System.out.println(number4);
+        System.out.println(casting);
     }
 }
