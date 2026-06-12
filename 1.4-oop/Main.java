107d3e7dccdfdd9af009c19d20c7fd45f8087f13
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-12 18:58:33 -0500

    style: rename variables using camelCase

--- 1.4-oop/Main.java
+++ 1.4-oop/Main.java
@@ -2,20 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         /* Inicialize 2 BankAccount objets */
-        BankAccount BankAccount1 = new BankAccount("samuel", 0);
-        BankAccount BankAccount2 = new BankAccount("samuel", 0);
+        BankAccount bankAccount1 = new BankAccount("samuel", 0);
+        BankAccount bankAccount2 = new BankAccount("samuel", 0);
         
         /* Use deposit method */
-        BankAccount1.deposit(-2); // Validate a logic amount
-        BankAccount2.deposit(2500); // Use in a correct way
+        bankAccount1.deposit(-2); // Validate a logic amount
+        bankAccount2.deposit(2500); // Use in a correct way
 
         /* Use printBalance method */
-        BankAccount1.printBalance();
-        BankAccount2.printBalance();
+        bankAccount1.printBalance();
+        bankAccount2.printBalance();
 
         /* Use withdraw method */
-        BankAccount1.withdraw(5); // Validate enough balance
-        BankAccount2.withdraw(2000); // Use in a correct way
+        bankAccount1.withdraw(5); // Validate enough balance
+        bankAccount2.withdraw(2000); // Use in a correct way
 
         /* Use the static attribute */
         System.out.println(BankAccount.accountCounter);
