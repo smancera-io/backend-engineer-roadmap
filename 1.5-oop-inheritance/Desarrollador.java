96390799bf9c4e7a451a6aa051f291f8221506a9
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-13 22:44:36 -0500

    docs: add simple comments in Gerente and Desarrollador class

--- 1.5-oop-inheritance/Desarrollador.java
+++ 1.5-oop-inheritance/Desarrollador.java
@@ -1,16 +1,20 @@
 public class Desarrollador extends Empleado {
     
+    /* Add new class attribute */
     private double degreeBonus;
 
+    /* Create super constructor method*/
     public Desarrollador (String name, int ID, double salary, double degreeBonus) {
         super(name, ID, salary);
         this.degreeBonus = degreeBonus;
     }
 
+    /* Create getter methods */
     public double getDegreeBonus () {
         return degreeBonus;
     }
 
+    /* Apply override to the father's method */
     @Override
     public double calculateSalary () {
         return salary + degreeBonus;
