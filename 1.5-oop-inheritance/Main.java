81aba0e0043751826441f8c28da98c6baf9f0d98
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-14 16:33:52 -0500

    feat: initialize Gerente objects and add to Empelados list

--- 1.5-oop-inheritance/Main.java
+++ 1.5-oop-inheritance/Main.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        List <Empleado> empleados = new ArrayList<>();
+        Gerente gerente1 = new Gerente("Daniel", 1, 3500000, 500000);
+        Gerente gerente2 = new Gerente("Esteban", 2, 4500000, 390000);
+        Gerente gerente3 = new Gerente("Samuel", 3, 3000000, 550000);
+        Gerente gerente4 = new Gerente("Nicolas", 4, 2700000, 450000);
+        Gerente gerente5 = new Gerente("Juan", 5, 3800000, 600000);
+        empleados.add(gerente1);
+        empleados.add(gerente2);
+        empleados.add(gerente3);
+        empleados.add(gerente4);
+        empleados.add(gerente5);
+    }
 }
