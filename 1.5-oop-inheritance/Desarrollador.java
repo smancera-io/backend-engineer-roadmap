bac7a808f390e2c71d0a6a6cfcff7f204e5437d9
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-13 22:37:57 -0500

    feat: apply basic inheritance principle with 'extends' and superconstructor concep

--- 1.5-oop-inheritance/Desarrollador.java
+++ 1.5-oop-inheritance/Desarrollador.java
@@ -1,3 +1,13 @@
-public class Desarrollador {
+public class Desarrollador extends Empleado {
     
+    private double degreeBonus;
+
+    public Desarrollador (String name, int ID, double salary, double degreeBonus) {
+        super(name, ID, salary);
+        this.degreeBonus = degreeBonus;
+    }
+
+    public double getDegreeBonus () {
+        return degreeBonus;
+    }
 }
--- 1.5-oop-inheritance/Gerente.java
+++ 1.5-oop-inheritance/Gerente.java
@@ -1,3 +1,13 @@
-public class Gerente {
-    
+public class Gerente extends Empleado {
+
+    private double manageBonus;
+
+    public Gerente (String name, int ID, double salary, double manageBonus) {
+        super(name, ID, salary);
+        this.manageBonus = manageBonus;
+    }
+
+    public double getManageBonus () {
+        return manageBonus;
+    }
 }
