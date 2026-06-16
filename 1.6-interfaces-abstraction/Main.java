3d428f77b16ad94f0195f4712a370078eaf55561
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-15 20:01:41 -0500

    fix: update empleados list implementation with the new class

--- 1.6-interfaces-abstraction/Main.java
+++ 1.6-interfaces-abstraction/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         /* Create empleados List */
-        List <Empleado> empleados = new ArrayList<>();
+        Equipo equipo = new Equipo();
 
         /* Initialize gerente objects */
         Gerente gerente1 = new Gerente("Daniel", 1, 3500000, 500000);
@@ -14,11 +14,11 @@ public class Main {
         Gerente gerente5 = new Gerente("Juan", 5, 3800000, 600000);
 
         /* Add gerente objects to empleados List */
-        empleados.add(gerente1);
-        empleados.add(gerente2);
-        empleados.add(gerente3);
-        empleados.add(gerente4);
-        empleados.add(gerente5);
+        equipo.addEmpleados(gerente1);
+        equipo.addEmpleados(gerente2);
+        equipo.addEmpleados(gerente3);
+        equipo.addEmpleados(gerente4);
+        equipo.addEmpleados(gerente5);
 
         /* Initialize desarrollador objects  */
         Desarrollador desarrollador1 = new Desarrollador("David", 6, 3500000, 500000);
@@ -28,22 +28,22 @@ public class Main {
         Desarrollador desarrollador5 = new Desarrollador("Jesus", 10, 3800000, 600000);
 
         /* Add desarrollador objects to empleados list */
-        empleados.add(desarrollador1);
-        empleados.add(desarrollador2);
-        empleados.add(desarrollador3);
-        empleados.add(desarrollador4);
-        empleados.add(desarrollador5);
+        equipo.addEmpleados(desarrollador1);
+        equipo.addEmpleados(desarrollador2);
+        equipo.addEmpleados(desarrollador3);
+        equipo.addEmpleados(desarrollador4);
+        equipo.addEmpleados(desarrollador5);
 
         /* Print results of polymorphis */
-        for (Empleado empleado : empleados) {
+        /*for (Empleado empleado : empleados) {
             if (empleado.getID() == 10){
                 /* Use instanceof */
-                if (empleado instanceof Desarrollador) {
+                /*if (empleado instanceof Desarrollador) {
                     Desarrollador desarrollador9 = (Desarrollador) empleado;
                     System.out.println(desarrollador9.getDegreeBonus());
                 }
             }
             System.out.println(empleado.calculateSalary());
-        }
+        }*/
     }
 }
