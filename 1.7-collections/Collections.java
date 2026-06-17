6734401abf1dbe8a75891b2df35531fd30ccb47f
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-16 22:16:31 -0500

    feat: create ArrayList and LinkedList

---
+++ 1.7-collections/Collections.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        
+        /* Create ArrayList */
+        ArrayList <Persona> personasArray = new ArrayList<>();
+
+        /* Create LinkedList */
+        LinkedList <Persona> personasLinked = new LinkedList<>();
+    }
+}
