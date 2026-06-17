2745467ff896c8927f5b6942c8d9762bde487b16
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-17 18:58:54 -0500

    feat: initialize set data structures

--- 1.7-collections/Set.java
+++ 1.7-collections/Set.java
@@ -1,3 +1,17 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 public class Set {
-    
+    public static void main(String[] args) {
+        
+        /* Create a HashSet */
+        HashSet <Persona> personasHash = new HashSet<>();
+
+        /* Create a TreeSet */
+        TreeSet <Persona> personasTree = new TreeSet<>();
+
+        /* Create a LinkedHashSet */
+        LinkedHashSet <Persona> personasLinkedHash = new LinkedHashSet<>();
+    }
 }
