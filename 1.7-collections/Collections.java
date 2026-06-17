d6d84c366974caf0285eeff6310f9ccaf8eb3a7b
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-16 22:24:12 -0500

    feat: use get method and print it result

--- 1.7-collections/Collections.java
+++ 1.7-collections/Collections.java
@@ -24,5 +24,9 @@ public class Collections {
         personasLinked.add(new Persona("Juan", 3147522003L, "juan@gmail.com", 71));
         personasLinked.add(new Persona("David", 3132599677L, "david@gmail.com", 32));
         personasLinked.add(new Persona("Carlos", 3102554190L, "carlos@gmail.com", 48));
+
+        /* Obtain a value through index */
+        System.out.println(personasArray.get(1));
+        System.out.println(personasLinked.get(1));
     }
 }
