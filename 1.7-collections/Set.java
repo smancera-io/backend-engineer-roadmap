d07f06d6e4e1811a71bdf57a30cfe66774f93e97
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-17 19:02:09 -0500

    feat: add values to the data structures

--- 1.7-collections/Set.java
+++ 1.7-collections/Set.java
@@ -13,5 +13,29 @@ public class Set {
 
         /* Create a LinkedHashSet */
         LinkedHashSet <Persona> personasLinkedHash = new LinkedHashSet<>();
+
+        /* Add values to personasHash */
+        personasHash.add(new Persona("Samuel", 3229884569L, "samuel@gmail.com", 15));
+        personasHash.add(new Persona("Juan", 3147522003L, "juan@gmail.com", 71));
+        personasHash.add(new Persona("David", 3132599677L, "david@gmail.com", 32));
+        personasHash.add(new Persona("Carlos", 3102554190L, "carlos@gmail.com", 48));
+        personasHash.add(new Persona("Esteban", 3204599963L, "esteban@gmail.com",20));
+
+        /* Add values to personasTree */
+        personasTree.add(new Persona("Samuel", 3229884569L, "samuel@gmail.com", 15));
+        personasTree.add(new Persona("Esteban", 3204599963L, "esteban@gmail.com",20));
+        personasTree.add(new Persona("Juan", 3147522003L, "juan@gmail.com", 71));
+        personasTree.add(new Persona("David", 3132599677L, "david@gmail.com", 32));
+        personasTree.add(new Persona("Carlos", 3102554190L, "carlos@gmail.com", 48));
+
+        /* Add values to LinkedHashSet */
+        personasLinkedHash.add(new Persona("Samuel", 3229884569L, "samuel@gmail.com", 15));
+        personasLinkedHash.add(new Persona("Esteban", 3204599963L, "esteban@gmail.com",20));
+        personasLinkedHash.add(new Persona("Juan", 3147522003L, "juan@gmail.com", 71));
+        personasLinkedHash.add(new Persona("David", 3132599677L, "david@gmail.com", 32));
+        personasLinkedHash.add(new Persona("Carlos", 3102554190L, "carlos@gmail.com", 48));
+
+
+
     }
 }
