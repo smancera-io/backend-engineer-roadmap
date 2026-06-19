415c66b19e2400d2ee47a0853719f1600f7fe782
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-18 21:47:00 -0500

    feat: create foreach to obtain the word's frequency

--- 1.7-collections/Map.java
+++ 1.7-collections/Map.java
@@ -2,10 +2,15 @@ import java.util.HashMap;
 
 public class Map {
     public static void main(String[] args) {
-        
-        HashMap <String, Integer> frequence = new HashMap<>();
+
+        HashMap<String, Integer> frequence = new HashMap<>();
 
         String phrase = "Juan tuvo un tubo y el tubo que tuvo se le rompió y para recuperar el tubo que tuvo tuvo que comprar un tubo igual al tubo que tuvo y rompió";
-        String [] pieces = phrase.split(" ");
+        String[] pieces = phrase.split(" ");
+        for (String piece : pieces) {
+            frequence.merge(piece, 1, Integer::sum);
+        }
+
+        System.out.println(frequence);
     }
 }
