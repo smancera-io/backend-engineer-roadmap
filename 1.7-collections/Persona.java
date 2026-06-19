15befc8e4fa6288d0d3fb809269cc02b520f6e4d
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-18 20:55:51 -0500

    feat: add hashCode and equals method to Persona class

--- 1.7-collections/Persona.java
+++ 1.7-collections/Persona.java
@@ -14,6 +14,34 @@ class Persona implements Comparable {
         this.age = age;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (phoneNumber ^ (phoneNumber >>> 32));
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        if (phoneNumber != other.phoneNumber)
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
     /* Create getter methods */
     public String getName () {
         return name;
