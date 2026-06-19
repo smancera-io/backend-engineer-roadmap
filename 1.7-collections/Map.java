feaf33089d67f795729f854791f523cb3074aaaf
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-18 21:51:27 -0500

    chore: rename HashMap collection

--- 1.7-collections/Map.java
+++ 1.7-collections/Map.java
@@ -4,7 +4,7 @@ public class Map {
     public static void main(String[] args) {
 
         /* Create a HashMap */
-        HashMap<String, Integer> frequence = new HashMap<>();
+        HashMap<String, Integer> frequency = new HashMap<>();
 
         /* Initialize a String phrase */
         String phrase = "Juan tuvo un tubo y el tubo que tuvo se le rompió y para recuperar el tubo que tuvo tuvo que comprar un tubo igual al tubo que tuvo y rompió";
@@ -14,10 +14,10 @@ public class Map {
 
         /* Add keys and values to frequency HashMap */
         for (String piece : pieces) {
-            frequence.merge(piece, 1, Integer::sum);
+            frequency.merge(piece, 1, Integer::sum);
         }
 
         /* Print result */
-        System.out.println(frequence);
+        System.out.println(frequency);
     }
 }
