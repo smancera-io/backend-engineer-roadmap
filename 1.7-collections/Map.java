45c6eb8c2c2faf04f36d422031ffd7b75875c607
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-18 21:22:10 -0500

    feat: create HashMap, initialize a string phrase and split it to work

--- 1.7-collections/Map.java
+++ 1.7-collections/Map.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+
 public class Map {
     public static void main(String[] args) {
         
+        HashMap <String, Integer> frequence = new HashMap<>();
+
+        String phrase = "Juan tuvo un tubo y el tubo que tuvo se le rompió y para recuperar el tubo que tuvo tuvo que comprar un tubo igual al tubo que tuvo y rompió";
+        String [] pieces = phrase.split(" ");
     }
 }
