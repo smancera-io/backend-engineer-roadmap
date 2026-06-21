4cfe72fca30117eaca1f7f82e22abf3a588aaa65
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-20 21:13:09 -0500

    feat: add protected attributes in Vehiculo class

---
+++ 2-excercises/Vehiculo.java
@@ -0,0 +1,8 @@
+public class Vehiculo {
+
+    protected String placa;
+    protected String marca;
+    protected String modelo;
+    protected int añoFabricacion;
+    protected double kilometraje;
+}
