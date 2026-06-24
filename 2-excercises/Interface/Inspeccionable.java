71e899d8606c44034beab6dc9dc91080cd0ac44d
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-23 21:29:37 -0500

    feat: add default method interface 'generarCertificado'

--- 2-excercises/Interface/Inspeccionable.java
+++ 2-excercises/Interface/Inspeccionable.java
@@ -1,5 +1,14 @@
 package Interface;
 
+import java.time.LocalDate;
+
 public interface Inspeccionable {
     public boolean realizarInspeccion ();
+
+    public default String generarCertificado () {
+        boolean aprobado = realizarInspeccion();
+        String estado = aprobado ? "APROBADO" : "RECHAZADO";
+        int id = (int) (Math.random()*100000) + 1;
+        return String.format("[CERTIFICADO]\nNº: %d\nFecha: %s\nResultado: %s", id, LocalDate.now(), estado);
+    }
 }
