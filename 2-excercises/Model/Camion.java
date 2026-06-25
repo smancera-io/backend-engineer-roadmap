d455f7d67a10429c188936aa06e43325e072e978
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-24 21:29:37 -0500

    fix: correct Camion class with new attributes

--- 2-excercises/Model/Camion.java
+++ 2-excercises/Model/Camion.java
@@ -1,33 +1,53 @@
 package Model;
 
+import ENUM.EstadoVehiculo;
 import Interface.Asegurable;
 
 public class Camion extends Vehiculo implements Asegurable{
 
     /* New class attribute */
     private double capacidadCarga;
+    private int numeroEjes;
+    private double valorComercial;
 
     /* Superconstructor method */
-    public Camion(String placa, String marca, String modelo, int añoFabricacion, double kilometraje, double capacidadCarga) {
-        super(placa, marca, modelo, añoFabricacion, kilometraje);
+    public Camion(String placa, String marca, String modelo, int añoFabricacion, double kilometraje,
+            EstadoVehiculo estado, double capacidadCarga, int numeroEjes, double valorComercial) {
+        super(placa, marca, modelo, añoFabricacion, kilometraje, estado);
         this.capacidadCarga = capacidadCarga;
+        this.numeroEjes = numeroEjes;
+        this.valorComercial = valorComercial;
     }
     
     /* Inherited method */
     @Override
-    public double calcularCostoMantenimiento(double precioAceite, double precioFiltro, double manoObra) {
-        double costo = (precioAceite + precioAceite + manoObra) * (capacidadCarga * 0.5);
-        return costo;
+    public double calcularCostoMantenimiento() {
+        return 500.0
+            + (getKilometraje() * 0.10)
+            + (capacidadCarga * 100.0)
+            + (calcularAntiguedad() * 80.0);
     }
 
     /* Setter method */
     public void setCapacidadCarga (double capacidadCarga) {
         this.capacidadCarga = capacidadCarga;
     }
+    public void setNumeroEjes(int numeroEjes) {
+        this.numeroEjes = numeroEjes;
+    }
+    public void setValorComercial(double valorComercial) {
+        this.valorComercial = valorComercial;
+    }
 
     /* Getter method */
     public double getCapacidadCarga () {
         return capacidadCarga;
+    }
+    public int getNumeroEjes() {
+        return numeroEjes;
+    }
+    public double getValorComercial() {
+        return valorComercial;
     }
 
     /* Asegurable interface method */
