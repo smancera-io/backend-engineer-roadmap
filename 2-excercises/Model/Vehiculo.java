742991cee48a18d27c245a141c356292e61befeb
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-24 21:03:17 -0500

    feat: add hashCode method to Vehiculo class

--- 2-excercises/Model/Vehiculo.java
+++ 2-excercises/Model/Vehiculo.java
@@ -87,4 +87,9 @@ public abstract class Vehiculo {
         Vehiculo other = (Vehiculo) o;
         return Objects.equals(placa, other.placa);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(placa);
+    }
 }
