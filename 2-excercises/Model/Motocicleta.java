264ff51bf84a0d6e9bfdf38d4b1fc7f050cf5b85
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-24 21:41:40 -0500

    chore: remove unnecessary interface

--- 2-excercises/Model/Motocicleta.java
+++ 2-excercises/Model/Motocicleta.java
@@ -1,47 +1,35 @@
 package Model;
 
-import Interface.Asegurable;
+import ENUM.EstadoVehiculo;
 
-public class Motocicleta extends Vehiculo implements Asegurable{
+public class Motocicleta extends Vehiculo {
 
     /* New class attribute */
     private double cilindraje;
 
     /* Superconstructor method */
-    public Motocicleta(String placa, String marca, String modelo, int añoFabricacion, double kilometraje, double cilindraje) {
-        super(placa, marca, modelo, añoFabricacion, kilometraje);
+    public Motocicleta(String placa, String marca, String modelo, int añoFabricacion, double kilometraje,
+            EstadoVehiculo estado, double cilindraje) {
+        super(placa, marca, modelo, añoFabricacion, kilometraje, estado);
         this.cilindraje = cilindraje;
-        
     }
 
     /* Inherited method */
     @Override
     public double calcularCostoMantenimiento() {
         return 80.0
-            + (getKilometraje() * 0.02)
-            + (cilindraje * 0.10)
-            + (calcularAntiguedad() * 20.0);
-    }
-    
-    /* Setter method */
-    public void setCilindraje ( double cilindraje) {
-        this.cilindraje = cilindraje;
-    }
-    /* Getter method */
-    public double getCilindraje () {
-        return cilindraje;
+                + (getKilometraje() * 0.02)
+                + (cilindraje * 0.10)
+                + (calcularAntiguedad() * 20.0);
     }
 
-    /* Asegurable interface method */
-    @Override
-    public double calcularPrimaSeguro() {
-        int n = (int) cilindraje / 100;
-        double primaSeguro = switch (n) {
-            case 1, 2, 3 -> 150000;
-            case 4, 5, 6, 7 -> 250000;
-            case 8, 9, 10 -> 500000;
-            default -> 20000;
-        };
-    return primaSeguro * 1.05;
+    /* Setter method */
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    /* Getter method */
+    public double getCilindraje() {
+        return cilindraje;
     }
 }
