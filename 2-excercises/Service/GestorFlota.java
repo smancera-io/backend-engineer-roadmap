2a98023349faaa131c71ad02ec319d982964135c
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-24 21:43:37 -0500

    chore: create collections

--- 2-excercises/Service/GestorFlota.java
+++ 2-excercises/Service/GestorFlota.java
@@ -1,5 +1,14 @@
 package Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+import Model.Vehiculo;
+
 public class GestorFlota {
+
+    private final Map<String, Vehiculo> flota = new HashMap<>();
+    private final TreeSet<Vehiculo> flotaOrdenada = new TreeSet<>();
     
 }
