0ee6681545c84715b0f8681dc48b2e75b8335d23
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-24 21:22:42 -0500

    fix: add new attributes and modify constructor, setter, getter methods

--- 2-excercises/Model/Automovil.java
+++ 2-excercises/Model/Automovil.java
@@ -1,18 +1,41 @@
 package Model;
 
+import ENUM.EstadoVehiculo;
 import Interface.Asegurable;
 
 public class Automovil extends Vehiculo implements Asegurable {
 
-    /* Superconstructor method */
-    public Automovil(String placa, String marca, String modelo, int añoFabricacion, double kilometraje) {
-        super(placa, marca, modelo, añoFabricacion, kilometraje);
+    /* New attributes class */
+    private int numeroPuertas;
+    private double valorComercial;
 
+    /* Superconstructor method */
+    public Automovil(String placa, String marca, String modelo, int añoFabricacion, double kilometraje,
+            EstadoVehiculo estado, int numeroPuertas, double valorComercial) {
+        super(placa, marca, modelo, añoFabricacion, kilometraje, estado);
+        this.numeroPuertas = numeroPuertas;
+        this.valorComercial = valorComercial;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public double getValorComercial() {
+        return valorComercial;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public void setValorComercial(double valorComercial) {
+        this.valorComercial = valorComercial;
     }
 
     /* Inherited method */
     @Override
-    public double calcularCostoMantenimiento(double costoAceite, double costoFiltro, double manoObra) {
+    public double calcularCostoMantenimiento() {
         double costo = (costoAceite + costoFiltro + manoObra) * kilometraje / 10000;
         return costo;
     }
