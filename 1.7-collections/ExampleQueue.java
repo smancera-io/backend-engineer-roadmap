30d575a39ba3caf65f9077c1a627bcf7f8629d5f
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-25 21:48:41 -0500

    chore: rename file to solve conflicts

---
+++ 1.7-collections/ExampleQueue.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExampleQueue {
+    public static void main(String[] args) {
+        /* Create a FIFO structure */
+        Queue <String> list = new LinkedList<>();
+        
+    }
+}
