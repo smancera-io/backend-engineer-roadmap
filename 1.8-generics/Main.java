cc981a905f44f9b54699e048b42aa714ce1470be
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-26 22:46:57 -0500

    docs: add guided comments in Main class

--- 1.8-generics/Main.java
+++ 1.8-generics/Main.java
@@ -19,28 +19,35 @@ public class Main {
         System.out.println(names.getItem());
         System.out.println(numbers.getItem());
 
-        /* Initialize a List  */
+        /* Initialize a String List  */
         List<String> listaNombres = new ArrayList<>();
         listaNombres.add("Samuel");
         listaNombres.add("Elena");
         listaNombres.add("Lucas");
 
-        /* Use generic methods from Caja class */
+        /* Use generic methods from Caja class with String values*/
         Caja.printItems(listaNombres);
         System.out.println(Caja.countItems(listaNombres));
 
         /* Use Caja class with Persona class */
+        /* Initialize a Caja class with Persona argument (single instance) */
         Caja<Persona> persona = new Caja<>();
-        persona.setItem(new Persona("Daniel", 322947889852L, "daniel@gmail.com", 52));
-        System.out.println(persona.getItem());
-        List <Persona> personas = new ArrayList<>();
 
+        /* Add Persona instance */
+        persona.setItem(new Persona("Daniel", 322947889852L, "daniel@gmail.com", 52));
+
+        /* Print Caja value */
+        System.out.println(persona.getItem());
+
+        /* Initialize a Persona List */
+        List <Persona> personas = new ArrayList<>();
         personas.add(new Persona("Samuel", 3229455210L, "samuel@gmail.com", 19));
         personas.add(new Persona("Esteban",3299874562L, "esteban@gmail.com", 45));
         personas.add(new Persona("Juan", 3134896522L, "juan@gmail.com", 8));
         personas.add(new Persona("David", 3478963201L, "david@gmail.com", 15));
         personas.add(new Persona("Nicolas", 3967584123L, "nicolas@gmail.com", 80));
 
+        /* Use generics methods from Caja class with Persona values */
         Caja.printItems(personas);
         System.out.println(Caja.countItems(personas));
     }
