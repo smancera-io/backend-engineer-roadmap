9e30a4df92cddd1369a08580282e94ca57095149
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-26 22:20:07 -0500

    docs: comment Main class

--- 1.8-generics/Main.java
+++ 1.8-generics/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        /* Initialize a Caja class with String argument*/
         Caja<String> names = new Caja<>();
+
+        /* Add String value */
         names.setItem("Samuel");
+
+        /* Initialize a new Caja class with Integer argument */
         Caja<Integer> numbers = new Caja<>();
+
+        /* Add Integer value */
         numbers.setItem(47);
+
+        /* Print Cajas class values */
         System.out.println(names.getItem());
         System.out.println(numbers.getItem());
     }
