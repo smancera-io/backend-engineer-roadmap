b29f9b01c88050c509c4fea7d85aa0b3755efe43
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-26 22:18:23 -0500

    feat: use generic Caja class with single Wrapper values

--- 1.8-generics/Main.java
+++ 1.8-generics/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Caja<String> names = new Caja<>();
+        names.setItem("Samuel");
+        Caja<Integer> numbers = new Caja<>();
+        numbers.setItem(47);
+        System.out.println(names.getItem());
+        System.out.println(numbers.getItem());
+    }
 }
