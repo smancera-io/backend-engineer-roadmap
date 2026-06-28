f798857346639d86c162614702693edc87e8cb78
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-27 22:12:05 -0500

    fix: move try block and add a new catch block

--- 1.9-exceptions/Main.java
+++ 1.9-exceptions/Main.java
@@ -1,26 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-
-        /* Inicialize 2 BankAccount objets */
-        BankAccount bankAccount1 = new BankAccount("samuel", 0);
-        BankAccount bankAccount2 = new BankAccount("samuel", 0);
-
-        /* Use deposit method */
-        bankAccount1.deposit(-2); // Validate a logic amount
-        bankAccount2.deposit(2500); // Use in a correct way
-
-        /* Use printBalance method */
-        bankAccount1.printBalance();
-        bankAccount2.printBalance();
-
-        /* Use withdraw method inside a tryCatch block */
+        /* Use try block */
         try {
-            bankAccount1.withdraw(5); // Validate enough balance
-            bankAccount2.withdraw(2000); // Test handle exception
+
+            /* Inicialize 2 BankAccount objets */
+            BankAccount bankAccount1 = new BankAccount("samuel", 0);
+            BankAccount bankAccount2 = new BankAccount("samuel", 0);
+
+            /* Use deposit method */
+            bankAccount1.deposit(-2); // Validate a logic amount and test handle exception
+            bankAccount2.deposit(2500); // Use in a correct way
+
+            /* Use printBalance method */
+            bankAccount1.printBalance();
+            bankAccount2.printBalance();
+
+            /* Use withdraw method */
+            bankAccount1.withdraw(5); // Validate enough balance and test handle exception
+            bankAccount2.withdraw(2000); // Use in a correct way
+
+            /* Add catch and finally blocks */
         } catch (SaldoInsuficienteException e) {
             System.out.println("Error:" + e.getMessage());
-        }
-        finally{
+        } catch (CantidadNegativaException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
             System.out.println("Fin de la sesion");
         }
 
