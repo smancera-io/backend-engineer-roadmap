0ae974008bdcce4843df724b77d057de0efc07f1
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-30 20:52:07 -0500

    feat: create hashMap in Main class

--- 1.11-equality-inmutability/Main.java
+++ 1.11-equality-inmutability/Main.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         
+        /* Create hashMap structure */
+        Map <Integer, String> hashMap = new HashMap<>();
     }
 }
