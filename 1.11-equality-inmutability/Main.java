61a68f042eb5e00881fd04777ee8a9437f133ab7
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-30 20:57:21 -0500

    fix: correct hashMap arguments and add one instance

--- 1.11-equality-inmutability/Main.java
+++ 1.11-equality-inmutability/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         
         /* Create hashMap structure */
-        Map <Integer, String> hashMap = new HashMap<>();
+        Map <Producto, String> hashMap = new HashMap<>();
 
         /* Create instances of Producto class */
         Producto harina = new Producto(1, "Harina");
         Producto leche = new Producto(1, "Leche");
+
+        /* Add object to hashMap */
+        hashMap.put(harina, "Encontrado producto Harina");
     }
 }
