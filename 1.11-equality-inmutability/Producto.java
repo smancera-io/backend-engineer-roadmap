fda5f570649c7ec11b71c0514877b3ab328b75e1
Samuel Mancera <samuelmancera.097@gmail.com>
2026-06-30 20:38:17 -0500

    feat: add equals and hashCode method using only id attribute

--- 1.11-equality-inmutability/Producto.java
+++ 1.11-equality-inmutability/Producto.java
@@ -6,4 +6,26 @@ public class Producto {
         this.id = id;
         this.name = name;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Producto other = (Producto) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 }
