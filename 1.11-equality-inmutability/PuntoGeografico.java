2f916807d533d89d4c13b8f4821948027bf979ac
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-01 20:31:44 -0500

    docs: add comments in final class 'PuntoGeografico'

--- 1.11-equality-inmutability/PuntoGeografico.java
+++ 1.11-equality-inmutability/PuntoGeografico.java
@@ -1,12 +1,15 @@
 public final class PuntoGeografico {
+    /* Private final attributes */
     private final double latitud;
     private final double longitud;
 
+    /* Constructor method */
     public PuntoGeografico (double latitud, double longitud) {
         this.latitud = latitud;
         this.longitud = longitud;
     }
 
+    /* Getter methods */
     public double getLatitud () {
         return latitud;
     }
