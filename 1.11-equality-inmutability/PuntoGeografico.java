d77c80db80e9b34f6385d8ee7e272956dbf857bb
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-01 20:30:18 -0500

    feat: add access modifier in constructor method

--- 1.11-equality-inmutability/PuntoGeografico.java
+++ 1.11-equality-inmutability/PuntoGeografico.java
@@ -2,8 +2,9 @@ public final class PuntoGeografico {
     private final double latitud;
     private final double longitud;
 
-    PuntoGeografico (double latitud, double longitud) {
+    public PuntoGeografico (double latitud, double longitud) {
         this.latitud = latitud;
         this.longitud = longitud;
     }
+
 }
