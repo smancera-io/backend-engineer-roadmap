eb5900979f8ac9d15eb9b7dca557931bf94a28c2
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-01 20:29:34 -0500

    feat: add final attributes in final class

--- 1.11-equality-inmutability/PuntoGeografico.java
+++ 1.11-equality-inmutability/PuntoGeografico.java
@@ -1,3 +1,9 @@
 public final class PuntoGeografico {
-    
+    private final double latitud;
+    private final double longitud;
+
+    PuntoGeografico (double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
 }
