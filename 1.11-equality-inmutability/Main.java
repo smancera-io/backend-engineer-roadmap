05ba8bbbe15e4748923ed0c6163830c6e5f499fd
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-01 21:20:14 -0500

    docs: add comments in inmutable part in Main class with corrections and new implementation

--- 1.11-equality-inmutability/Main.java
+++ 1.11-equality-inmutability/Main.java
@@ -29,25 +29,31 @@ public class Main {
         /* Print initial and inmutable value */
         System.out.println("Punto geografico inical: " + maldivas.getLatitud());
 
+        /* Share value with different methods */
         System.out.println("Compartiendo valor con metodos");
         procesarEnModuloA(maldivas);
         procesarEnModuloB(maldivas);
 
+        /* Use different structures */
         System.out.println("Compartiendo estructuras");
         List<PuntoGeografico> registroVuelos = new ArrayList<>();
         List<PuntoGeografico> catalogoHoteles = new ArrayList<>();
 
+        /* Add same data */
         registroVuelos.add(maldivas);
         catalogoHoteles.add(maldivas);
 
+        /* Show values */
         System.out.println("Vuelo programado a: " + registroVuelos.get(0).getLatitud());
         System.out.println("Hotel ubicado en: " + catalogoHoteles.get(0).getLatitud());
 
+        /* Show final values */
         System.out.println("Datos finales");
         System.out.println("Maldivas sigue siendo: " + maldivas.getLatitud() + ", " + maldivas.getLongitud());
         System.out.println("Garantía: Ninguna estructura o método pudo alterar el objeto original.");
     }
 
+    /* Methods that show it's impossible change PuntoGeografico values */
     private static void procesarEnModuloA(PuntoGeografico punto) {
         double calculo = punto.getLatitud() * 2;
         System.out.println("Leyendo coordenadas de forma segura.");
