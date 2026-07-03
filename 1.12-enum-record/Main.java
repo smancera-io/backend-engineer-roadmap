a57a6ee0a161d1462f9e61391bc8b08dcf8f9556
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-03 11:57:55 -0500

    feat: create LinkedList and add instances of Pedido

--- 1.12-enum-record/Main.java
+++ 1.12-enum-record/Main.java
@@ -1,9 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Pedido pedido1 = new Pedido(1, "Samuel", EstadoPedido.CREADO, 2100000);
-        Pedido pedido2 = new Pedido(2, "David", EstadoPedido.ENVIADO, 1750000);
-        Pedido pedido3 = new Pedido(3, "Juan", EstadoPedido.ENTREGADO, 570000);
-        Pedido pedido4 = new Pedido(4, "Sofia", EstadoPedido.EN_PROCESO, 890000);
-        Pedido pedido5 = new Pedido(5, "Daniel", EstadoPedido.CANCELADO, 254000);
+        List <Pedido> listaPedidos = new LinkedList<>();
+        listaPedidos.add(new Pedido(1, "Samuel", EstadoPedido.CREADO, 2100000));
+        listaPedidos.add(new Pedido(2, "David", EstadoPedido.ENVIADO, 1750000));
+        listaPedidos.add(new Pedido(3, "Juan", EstadoPedido.ENTREGADO, 570000));
+        listaPedidos.add(new Pedido(4, "Sofia", EstadoPedido.EN_PROCESO, 890000));
+        listaPedidos.add(new Pedido(5, "Daniel", EstadoPedido.CANCELADO, 254000));
     }
 }
