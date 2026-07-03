a4888fe82bb765e712ed5c1d2c40f5f7f8b335a2
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-02 22:33:11 -0500

    docs: add comments in Enum class

--- 1.12-enum-record/EstadoPedido.java
+++ 1.12-enum-record/EstadoPedido.java
@@ -6,16 +6,20 @@ public enum EstadoPedido {
     ENTREGADO("El cliente ya recibio su pedido"),
     CANCELADO("El pedido fue cancelado y ya no se procesara");
 
+    /* Private attribute */
     private String descripcion;
 
+    /* Constructor method */
     private EstadoPedido(String descripcion) {
         this.descripcion = descripcion;
     }
 
+    /* Getter method */
     public String getDescripcion () {
         return descripcion;
     }
 
+    /* Boolean method */
     public boolean esFinalizado () {
         return this == ENTREGADO ||  this == CANCELADO;   
     }
