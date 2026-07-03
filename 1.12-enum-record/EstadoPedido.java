757b23f085c870649ef6d9964d3570a0b0813ee3
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-02 22:23:47 -0500

    feat: add 'descripcion' attribute in Enum class

--- 1.12-enum-record/EstadoPedido.java
+++ 1.12-enum-record/EstadoPedido.java
@@ -1,8 +1,18 @@
 public enum EstadoPedido {
     /* Posibles estados enum */
-    CREADO,
-    EN_PROCESO,
-    ENVIADO,
-    ENTREGADO,
-    CANCELADO;
+    CREADO("El pedido ha sido creado con exito"),
+    EN_PROCESO("El pedido esta en preparacion"),
+    ENVIADO("El pedido esta en camino al destino"),
+    ENTREGADO("El cliente ya recibio su pedido"),
+    CANCELADO("El pedido fue cancelado y ya no se procesara");
+
+    private String descripcion;
+
+    private EstadoPedido(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion () {
+        return descripcion;
+    }
 }
