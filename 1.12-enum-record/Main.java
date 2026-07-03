8ced58083af5b273d25f04a8f09ed8f712db9430
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-03 11:55:08 -0500

    feat: create instances of Pedido class in Main

--- 1.12-enum-record/Main.java
+++ 1.12-enum-record/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Pedido pedido1 = new Pedido(1, "Samuel", EstadoPedido.CREADO, 2100000);
+        Pedido pedido2 = new Pedido(2, "David", EstadoPedido.ENVIADO, 1750000);
+        Pedido pedido3 = new Pedido(3, "Juan", EstadoPedido.ENTREGADO, 570000);
+        Pedido pedido4 = new Pedido(4, "Sofia", EstadoPedido.EN_PROCESO, 890000);
+        Pedido pedido5 = new Pedido(5, "Daniel", EstadoPedido.CANCELADO, 254000);
     }
 }
