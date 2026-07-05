caaac39e9605c5432d3178a02b5349c0ea80293f
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-04 21:27:34 -0500

    style: add format to the code lines

--- 1.13-lambda-functional-interfaces/Main.java
+++ 1.13-lambda-functional-interfaces/Main.java
@@ -2,13 +2,15 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        Predicate <Persona> mayorEdad = persona -> persona.getAge() > 18;
-        Function <List<Persona>, List<String>> listaEmail = lista -> lista.stream().map(Persona :: getEmail).collect(Collectors.toList());
+        Predicate<Persona> mayorEdad = persona -> persona.getAge() > 18;
+        Function<List<Persona>, List<String>> listaEmail = lista -> lista.stream().map(Persona::getEmail)
+                .collect(Collectors.toList());
     }
 }
