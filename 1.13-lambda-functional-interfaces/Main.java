28ba2c0cc0a572dcf079bee9ce2e67f743e91972
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-04 21:34:40 -0500

    feat: add Consumer lambda expression

--- 1.13-lambda-functional-interfaces/Main.java
+++ 1.13-lambda-functional-interfaces/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -12,5 +13,6 @@ public class Main {
         Predicate<Persona> mayorEdad = persona -> persona.getAge() > 18;
         Function<List<Persona>, List<String>> listaEmail = lista -> lista.stream().map(Persona::getEmail)
                 .collect(Collectors.toList());
+        Consumer<Persona> imprimirPersona = persona -> System.out.println(persona);
     }
 }
