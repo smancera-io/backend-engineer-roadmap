2611fef7bdfeb1d67d0522a455a8bca4b3e9f2c5
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-04 21:20:56 -0500

    feat: add Predicate lambda expression

--- 1.13-lambda-functional-interfaces/Main.java
+++ 1.13-lambda-functional-interfaces/Main.java
@@ -1,8 +1,11 @@
+import java.util.function.Predicate;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
+        Predicate <Persona> mayorEdad = persona -> persona.getAge() > 18;
     }
 }
