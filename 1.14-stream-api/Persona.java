16f286e6e2f0a9722a055614fff79bb0e4b3946e
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-05 15:01:03 -0500

    chore: copy Persona class

---
+++ 1.14-stream-api/Persona.java
@@ -0,0 +1,125 @@
+import java.util.List;
+import java.util.Optional;
+
+class Persona implements Comparable<Persona> {
+
+    /* Create attributes */
+    private String name;
+    private long phoneNumber;
+    private String email;
+    private Integer age;
+
+    /* Create constructor method */
+    public Persona(String name, long phoneNumber, String email, Integer age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.age = age;
+    }
+
+    /* Default constructor of an instance */
+    public Persona () {
+        this.name = "Samuel";
+        this.phoneNumber =3559633152L;
+        this.email = "samuel@gmail.com";
+        this.age = 20;
+    }
+
+    /* Modify hashCode method to recognize values better */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (phoneNumber ^ (phoneNumber >>> 32));
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    /* Modify equals method to prevent and confirm duplicate values */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        if (phoneNumber != other.phoneNumber)
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
+    /* Modify toString method to improve output object */
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone number: " + phoneNumber + ", Email: " + email + ", Age : " + age + "\n";
+    }
+
+    /* Create getter methods */
+    public String getName() {
+        return name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    /* Create setter methods */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /* Modify compareTo method to use TreeSet collection */
+    @Override
+    public int compareTo(Persona persona) {
+        return this.name.compareTo(persona.name);
+    }
+
+    /* Static method to searchByName */
+    public static Optional<Persona> buscarPersonaPorNombre(String name, List<Persona> personas) {
+        if (name == null) {
+            return Optional.empty();
+        }
+        for (Persona persona : personas) {
+            if (persona.getName().equals(name)) {
+                return Optional.of(persona);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /* Method to check if age is more or equals than 18 */
+    public boolean esMayor () {
+        if (age >= 18){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
