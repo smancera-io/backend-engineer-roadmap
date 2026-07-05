cc44ee21695381b9bf6bbcbeb002a428aa01123c
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-04 21:26:50 -0500

    feat: add Function lambda expression

--- 1.13-lambda-functional-interfaces/Main.java
+++ 1.13-lambda-functional-interfaces/Main.java
@@ -1,5 +1,7 @@
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
-
+import java.util.stream.Collectors;
 /**
  * Main
  */
@@ -7,5 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         Predicate <Persona> mayorEdad = persona -> persona.getAge() > 18;
+        Function <List<Persona>, List<String>> listaEmail = lista -> lista.stream().map(Persona :: getEmail).collect(Collectors.toList());
     }
 }
