8d7a1e2a0d6aa514da12dcbb060b1d003ffd229b
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-04 21:39:04 -0500

    feat: add Supplier lambda expression

--- 1.13-lambda-functional-interfaces/Main.java
+++ 1.13-lambda-functional-interfaces/Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 /**
@@ -14,5 +15,6 @@ public class Main {
         Function<List<Persona>, List<String>> listaEmail = lista -> lista.stream().map(persona -> persona.getEmail())
                 .collect(Collectors.toList());
         Consumer<Persona> imprimirPersona = persona -> System.out.println(persona);
+        Supplier<Persona> crearDefaultPersona = () -> new Persona("Samuel", 3559633152L, "samuel@gmail.com", 20);
     }
 }
