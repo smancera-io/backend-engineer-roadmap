d3c0b53780c480e60b33a1473708d8a3f4855873
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-06 17:59:37 -0500

    chore: delete fatal excersice

--- 4-excercises/Exception/VehiculoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package Exception;
-
-public class VehiculoNoEncontradoException extends RuntimeException {
-    /* Create method to modify exception message */
-    public VehiculoNoEncontradoException(String placa) {
-        super("Vehículo con placa '" + placa + "' no encontrado en la flota.");
-    }
-}
