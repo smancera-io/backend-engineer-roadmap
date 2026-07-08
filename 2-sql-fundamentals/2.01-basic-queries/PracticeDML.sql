cb03dd48b6173de170cca5194316094630e3e9f5
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-07 20:45:14 -0500

    chore: create 'employee' table

--- 2-sql-fundamentals/2.01-basic-queries/PracticeDML.sql
+++ 2-sql-fundamentals/2.01-basic-queries/PracticeDML.sql
@@ -1,2 +1,12 @@
 -- Active: 1783470162528@@127.0.0.1@3306@practice_sql
 CREATE DATABASE practice_sql;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10,2),
+    city VARCHAR(50),
+    entry_date DATE,
+    PRIMARY KEY(id)
+);
