ea95f2616d4c395bc6727bde20d351e310a6b502
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-09 21:24:51 -0500

    feat: COMPLEX-JOIN to get pairs of developers in the same project

--- 2-sql-fundamentals/2.02-joins/Joins.sql
+++ 2-sql-fundamentals/2.02-joins/Joins.sql
@@ -19,3 +19,15 @@ FROM developer dev
 JOIN person p ON dev.id = p.id
 LEFT JOIN assignment a ON a.developer = dev.id
 WHERE a.developer IS NULL;
+
+SELECT 
+    p.name AS proyect_name,
+    p.start_date, 
+    CONCAT (pe1.name, " ", pe1.last_name) AS developer1,
+    CONCAT (pe2.name, " ", pe2.last_name) AS developer2
+FROM assignment a1
+JOIN assignment a2 ON a1.proyect = a2.proyect
+    AND a1.id < a2.id
+JOIN proyect p ON a1.proyect = p.id
+JOIN person pe1 ON a1.developer = pe1.id
+JOIN person pe2 ON a2.developer = pe2.id;
