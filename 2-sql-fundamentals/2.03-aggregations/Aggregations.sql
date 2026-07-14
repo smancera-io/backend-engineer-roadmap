2782e531c869599c53156444631c00dd7c948a61
Samuel Mancera <samuelmancera.097@gmail.com>
2026-07-14 13:28:15 -0500

    feat: query to get developers with salary increase

--- 2-sql-fundamentals/2.03-aggregations/Aggregations.sql
+++ 2-sql-fundamentals/2.03-aggregations/Aggregations.sql
@@ -14,4 +14,16 @@ SELECT
     COUNT(dev.id) AS total_developers
 FROM developer_level devl
 LEFT JOIN developer dev ON devl.id = dev.level
-GROUP BY devl.id, devl.name
+GROUP BY devl.id, devl.name;
+
+SELECT 
+    CONCAT(per.name, ' ', per.last_name) AS developer,
+    MAX(dlh.salary) AS max_salary,
+    MIN(dlh.salary) AS min_salary,
+    (MAX(dlh.salary) - MIN(dlh.salary)) AS increment
+FROM developer_level_history dlh
+JOIN developer dev ON dlh.developer = dev.id
+JOIN person per ON dev.id = per.id
+GROUP BY dev.id, per.name, per.last_name
+HAVING (MAX(dlh.salary) - MIN(dlh.salary)) > 1000000
+ORDER BY increment DESC;
